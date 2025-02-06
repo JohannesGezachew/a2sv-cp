6962b54689fa93d55bf38044de0ef695770bbe4b
JohannesGezachew <97839635+JohannesGezachew@users.noreply.github.com>
2025-02-06 18:50:09 +0300

    Time: 3 ms (71.94%), Space: 18.5 MB (49.08%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,21 +3,26 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        zeros = []
         for row in range(len(matrix)):
             for col in range(len(matrix[0])):
                 if matrix[row][col] == 0:
-                    zeros.append([row,col])
+                    matrix[row][col] = " "
 
 
-        for point in zeros:
-            print(point)
-            x,y = point[0],point[1]
+        for row in range(len(matrix)):
+            for col in range(len(matrix[0])):
+                if matrix[row][col] == " ":
 
-            matrix[x] = [0] * (len(matrix[0]))
-
-            for i in range(len(matrix)):
-                matrix[i][y] = 0
+                    for i in range(len(matrix)):
+                        if matrix[i][col] != " ":
+                            matrix[i][col] = 0
+                        elif row == i :
+                            matrix[i][col] = 0
+                    for j in range(len(matrix[0])):
+                        if matrix[row][j] != " ":
+                            matrix[row][j] = 0
+                        elif col == j :
+                            matrix[row][j] = 0
 
         return matrix
 
