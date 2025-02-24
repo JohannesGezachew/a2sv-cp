655370ed587b4c710763536299f99db28470fdff
JohannesGezachew <97839635+JohannesGezachew@users.noreply.github.com>
2025-02-24 21:59:21 +0300

    Time: 23 ms (88.35%), Space: 20.2 MB (46.13%) - LeetHub

---
+++ 1472-design-browser-history/1472-design-browser-history.py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self, url: str):
+        self.url = url
+        self.prev = None
+        self.next = None
+
+class BrowserHistory:
+    def __init__(self, homepage: str):
+        self.current = Node(homepage)
+
+    def visit(self, url: str) -> None:
+        new_node = Node(url)
+        self.current.next = new_node
+        new_node.prev = self.current
+        self.current = new_node
+
+    def back(self, steps: int) -> str:
+        while steps > 0 and self.current.prev:
+            self.current = self.current.prev
+            steps -= 1
+        return self.current.url
+
+    def forward(self, steps: int) -> str:
+        while steps > 0 and self.current.next:
+            self.current = self.current.next
+            steps -= 1
+        return self.current.url
