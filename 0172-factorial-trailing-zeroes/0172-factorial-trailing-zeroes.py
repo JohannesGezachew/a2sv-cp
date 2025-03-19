83da91575508b46b3c391d9e174ed2dc33aebf75
JohannesGezachew <97839635+JohannesGezachew@users.noreply.github.com>
2025-03-20 01:04:18 +0300

    Time: 0 ms (100%), Space: 17.7 MB (82.63%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
@@ -1,13 +1,10 @@
 class Solution:
     def trailingZeroes(self, n: int) -> int:
-
-        if n < 5 :
-            return 0
-
-        k = int(log(n)//log(5))
         ans = 0
+        divisor = 5
 
-        for i in range(1,k+1):
-            ans += int(n //(5**i))
-            
+        while n//divisor:
+            ans += n//divisor
+            divisor *= 5
+        
         return ans
