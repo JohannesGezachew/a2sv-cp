9629b565465fc9c94c4d28de4faa730b77d307a0
JohannesGezachew <97839635+JohannesGezachew@users.noreply.github.com>
2025-04-08 02:09:02 +0300

    Time: 1726 ms (22.27%), Space: 501.5 MB (5.03%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -4,23 +4,13 @@ class Solution:
             return False
         
         target = sum(nums) // 2
-
-        memo = {}
-        
+        @cache
         def dp(idx, curr_sum):
             if idx == len(nums):
                 return curr_sum == target
             
-            if (idx, curr_sum) not in memo:
-                            
-                #take 
-                take = dp(idx + 1, curr_sum + nums[idx])
-                #dont_take
+            return dp(idx + 1, curr_sum + nums[idx]) or dp(idx + 1, curr_sum)
 
-                dont_take = dp(idx + 1, curr_sum)
-                memo[(idx, curr_sum)] = take or dont_take
-
-            return memo[(idx, curr_sum)]
         
         return dp(0, 0)
 
